1baa0e2deb49efce860f7aaef0e94964db95059e
Dean Hamstead <dean@fragfest.com.au>
2015-05-20 15:25:37 +1000

    bye

--- lib/Net/DNS/GslbFu/Checks/Replay.pm
+++ lib/Net/DNS/GslbFu/Checks/Replay.pm
@@ -8,6 +8,12 @@ package Net::DNS::GslbFu::Checks::Replay;
 use parent qw/ Net::DNS::GslbFu::ChecksBase /;
 
 # give back what we get
-sub run { return $_[1] }
+sub run {
+
+    my $self = shift;
+    my %opts = @_;
+    return $opts{result}
+
+}
 
 1
